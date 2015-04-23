96088cca227fb96351613e7d82bbaa157af10e36
Zane Swafford <zane@zaneswafford.com>
2015-04-22 22:30:28 -0500

    add gyroscope and parallax demo but commented out

--- Zane Swafford/CardTableViewController.swift
+++ Zane Swafford/CardTableViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import CoreMotion
 
 class CardTableViewController: UITableViewController {
+    var manager: CMMotionManager?
     var cards: [Card] = []
     var preventAnimation = Set<NSIndexPath>()
     
@@ -23,32 +25,33 @@ class CardTableViewController: UITableViewController {
         UITableView.appearance().backgroundColor = UIColor.clearColor()
         
         tableView.backgroundView = UIView()
+        
         let colorOne = UIColor(red:(120/255.0), green:(135/255.0), blue:(150/255.0), alpha:1.0)
         let colorTwo = UIColor(red:(57/255.0),  green:(79/255.0),  blue:(96/255.0),  alpha:1.0)
-        let colorThree = UIColor(hue:0.625, saturation:0.0, brightness:0.7, alpha:1.0)
-        let colorFour = UIColor(hue:0.625, saturation:0.0, brightness:0.4, alpha:1.0)
         
-        let colors =  [colorOne.CGColor, colorTwo.CGColor]//, colorThree.CGColor, colorFour.CGColor]
-        
-        let locations = [0.0, 1.0]//[0.0, 0.02, 0.99, 1.0];
+        let colors =  [colorOne.CGColor, colorTwo.CGColor]
+        let locations = [0.0, 1.0]
         
         var gradient = CAGradientLayer()
         gradient.frame = tableView.backgroundView!.bounds
-        gradient.colors = colors//[UIColor.blackColor().CGColor, UIColor.grayColor().CGColor]
+        gradient.colors = colors
         tableView.backgroundView!.layer.insertSublayer(gradient, atIndex: 0)
         gradient.locations = locations
-        tableView.backgroundView!.layoutSubviews()
         
         tableView.estimatedRowHeight = 300
         tableView.rowHeight = UITableViewAutomaticDimension
         
-        loadCards();
+        loadCards()
+        //setupGyroscope()
+        //setupParallax()
     }
 
+    // MARK: UITableView
     override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         if !preventAnimation.contains(indexPath) {
             preventAnimation.insert(indexPath)
             SwingingCellAnimator.animateIntro(cell)
+            cell.addParallaxWithOffsets(xOffset: -6, yOffset: -6)
         }
     }
     
@@ -75,6 +78,35 @@ class CardTableViewController: UITableViewController {
         return false
     }
     
+    // MARK: Gyroscope
+    func setupGyroscope() {
+        manager = CMMotionManager()
+        if manager!.deviceMotionAvailable {
+            manager!.deviceMotionUpdateInterval = 1.0/60.0
+            manager!.startDeviceMotionUpdatesToQueue(NSOperationQueue.mainQueue()) {
+                [weak self] (data: CMDeviceMotion!, error: NSError!) in
+                var rotation = (atan2(data.gravity.x, 1)) * -0.5
+                
+                println("ROTATION: \(rotation)\nGRAVITY.X: \(data.gravity.x)")
+                
+                println("ROTATION: \(rotation)")
+                for cell in self?.tableView.visibleCells() as! [CardTableViewCell] {
+                    cell.cardView.transform = CGAffineTransformMakeRotation(CGFloat(rotation))
+                }
+            }
+        } else {
+            println("No device motion 😦")
+        }
+    }
+    
+    // MARK: Parallax
+    func setupParallax() {
+        for cell in self.tableView.visibleCells() as! [CardTableViewCell] {
+            cell.addParallaxWithOffsets(xOffset: 20, yOffset: 20)
+        }
+    }
+    
+    // MARK: Rotation Animation
     override func willAnimateRotationToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
         if let bgGradient = tableView.backgroundView?.layer.sublayers[0] as? CALayer,
            let bgView = tableView.backgroundView{
