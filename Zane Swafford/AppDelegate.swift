0d4949f02e36cd021ad10098bd2c969f243552b5
Zane Swafford <zane@zaneswafford.com>
2015-04-22 20:15:23 -0500

    Changed font references to be in the project not external

--- Zane Swafford/AppDelegate.swift
+++ Zane Swafford/AppDelegate.swift
@@ -13,35 +13,10 @@ import CoreMotion
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    lazy var manager = CMMotionManager()
-    lazy var queue = NSOperationQueue()
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         UIApplication.sharedApplication().setStatusBarStyle(.LightContent, animated: true)
-        
-        if manager.gyroAvailable{
-            
-            if manager.gyroActive == false{
-                
-                manager.gyroUpdateInterval = 1.0 / 20.0
-                
-                manager.startGyroUpdatesToQueue(queue,
-                    withHandler: {(data: CMGyroData!, error: NSError!) in
-                        
-                        //println("Gyro Rotation x = \(data.rotationRate.x)")
-                        //println("Gyro Rotation y = \(data.rotationRate.y)")
-                        println("Gyro Rotation z = \(data.rotationRate.z)")
-                        
-                })
-                
-            } else {
-                println("Gyro is already active")
-            }
-            
-        } else {
-            println("Gyro isn't available")
-        }
-        
+
         return true
     }
 }
