1e154077a72091e984162a2c6111913868e50100
Zane Swafford <zane@zaneswafford.com>
2015-04-26 18:03:54 -0500

    updated Card model to only included needed fields

--- Zane Swafford/Card.swift
+++ Zane Swafford/Card.swift
@@ -11,27 +11,11 @@ import MapKit
 
 class Card {
     let coverPhotoName: String?
-    let photoNamesAlbum: Array<String>?
-    let location: CLLocationCoordinate2D?
     let text: String?
-    let url: NSURL?
     
     init(dictionary:NSDictionary) {
         coverPhotoName = dictionary["coverPhoto"] as? String
-        photoNamesAlbum = dictionary["photoAlbum"] as? Array<String>
-        
-        if let urlString = dictionary["url"] as? String {
-            url = NSURL(string: urlString)
-        } else {
-            url = nil
-        }
-        
-        if let locationTuple = dictionary["location"] as? (Double, Double) {
-            location = CLLocationCoordinate2DMake(locationTuple.0, locationTuple.1)
-        } else {
-            location = nil
-        }
-        
+
         if let escapedText = dictionary["text"] as? String {
             text = escapedText.stringByReplacingOccurrencesOfString("\\n", withString:"\n", options:nil, range:nil)
         } else {
